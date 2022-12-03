61850b6d36403a78b9a8f81a1138a3c64a4ad276
MarielTinaco <mariel.tinaco@analog.com>
2022-12-03 14:40:40 +0800

    First optimization

--- Day_03/main.py
+++ Day_03/main.py
@@ -1,5 +1,13 @@
 from pathlib import Path
 
+input_sample = \
+"""vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw
+"""
 alpha = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 def solve (input):  
@@ -31,13 +39,10 @@ def solve_2 (input):
             cluster.append(line)
             if (i + 1) % 3 == 0:
                 list_all.append(cluster)
+                common = list(set(cluster[0])&set(cluster[1])&set(cluster[2]))
+                val = alpha.index(common[0]) + 1
+                count += val
                 cluster = []
-    
-    for triple in list_all:
-        common = list(set(triple[0])&set(triple[1])&set(triple[2]))
-        val = alpha.index(common[0]) + 1
-
-        count += val
 
     return count
 
