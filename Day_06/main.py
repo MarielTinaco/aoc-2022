0869806acc24fbb21fd495b3706153de6f137834
MarielTinaco <mariel.tinaco@analog.com>
2022-12-07 22:12:21 +0800

    Day 7 answer

--- Day_06/main.py
+++ Day_06/main.py
@@ -1,8 +1,5 @@
 from pathlib import Path
-
-sample_input = \
-"""mjqjpqmgbljsphdztnvjfqwrcgsmlb
-"""
+import pytest
 
 def solve (input):
 
@@ -18,28 +15,41 @@ def solve (input):
 
     for i, char in enumerate(instr):
 
-        if len(contain) == 4:
-            actual_count_1 = i + 1
-
-        if len(contain) == 14:
-            actual_count_2 = i + 1
+        # if len(contain) == 14:
+        #     actual_count_2 = i + 1
 
         if char in contain:
             contain = set([])
         else:
             contain.add(char)
 
+        if len(contain) == 4:
+            actual_count_1 = i + 1
 
     ans1 = actual_count_1
     ans2 = actual_count_2
 
     return ans1, ans2
 
+test_input = \
+"""mjqjpqmgbljsphdztnvjfqwrcgsmlb
+"""
+expected_output = 7
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+        (test_input, expected_output),
+    ),
+)
+def test (input_s, expected) -> None:
+    ans1, ans2 = solve (input_s)
+    assert ans1 == expected
+
+
 if __name__ == "__main__":
-    try:
-        input = Path("Day_06/input.txt").read_text()
-    except:
-        input = sample_input
+
+    input = Path("Day_06/input.txt").read_text()
     
     p1, p2 = solve (input)
 
---
+++ Day_07/main.py
@@ -0,0 +1,86 @@
+from pathlib import Path
+import pytest
+from collections import defaultdict
+
+def solve(input):
+
+    ans1 = None
+    ans2 = None
+
+    file_sizes = defaultdict(int)
+    current_dir = Path("/")
+
+
+    for i, line in enumerate(input.split('\n')):
+      
+        if line != "":
+            splitted = line.split(" ")
+            if splitted[0] == "$":
+                if splitted[1] == "cd":
+                    current_dir = (current_dir / line[5:]).resolve()
+
+                elif splitted[1] == "ls":
+                    ...
+
+            elif splitted[0] == "dir":
+                ...
+            else:
+                size = int(line.split(" ")[0])
+                file_sizes[current_dir] += size
+                for parent in current_dir.parents:
+                    file_sizes[parent] += size
+    
+    ans1 = sum(v for v in file_sizes.values() if v <= 100000)
+
+
+    unused_space = file_sizes[Path("C:/")]
+    required = 30000000 - (70000000 - unused_space)
+
+    ans2 = min(v for v in file_sizes.values() if v >= required)
+
+    return ans2
+
+test_input = \
+"""$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k
+"""
+
+# expected_output = 95437
+expected_output = 24933642
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+        (test_input, expected_output),
+    ),
+)
+def test (input_s, expected) -> None:
+    assert solve (input_s) == expected
+
+
+if __name__ == "__main__":
+    INPUT_TEXT = Path(Path(__file__).parent / "input.txt").read_text()
+
+    ans = solve(INPUT_TEXT)
+    print("Part 2: ", ans)
